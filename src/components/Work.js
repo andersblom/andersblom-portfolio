621e6b7b68ecc0f8f54c878b98c777889b31e3f6
Anders Blom <anders@andersblom.dk>
2017-05-24 11:33:17 +0200

    fetching images from contentful

--- src/components/Work.js
+++ src/components/Work.js
@@ -2,27 +2,34 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 const contentful = require('contentful')
+const client = contentful.createClient({
+  space: 'eeluqlgcpzl3',
+  accessToken: '255fc48deab6dd5408e34a23b067f57642ca699179a369cd8a8ae5910cf37903'
+})
 
 /* 
 JSON output of everything:
 https://cdn.contentful.com/spaces/eeluqlgcpzl3/entries?access_token=255fc48deab6dd5408e34a23b067f57642ca699179a369cd8a8ae5910cf37903
 */
 
-const client = contentful.createClient({
-  space: 'eeluqlgcpzl3',
-  accessToken: '255fc48deab6dd5408e34a23b067f57642ca699179a369cd8a8ae5910cf37903'
-})
-
 var projectData;
 
 export default class Work extends Component {
     constructor() {
         super();
-        this.state = { itemsArray: null };
+        this.state = { itemsArray: null, assets: null };
     }
     componentDidMount() {
         client.getEntries().then(response => { 
             this.setState({ itemsArray: response.items });
+        }).catch(error => {
+            return error;
+        });    
+
+        //TODO: currently logs all images (url will be in array.fields.file.url)
+        client.getAssets().then(response => { 
+            console.log(response.items);
+            this.setState({ assets: response });
         }).catch(error => {
             return error;
         });        
