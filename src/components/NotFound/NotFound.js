8a2d411d01174920623cd62b3dd5023b09b5e9c2
Anders Blom <anders@andersblom.dk>
2017-06-20 15:59:12 +0200

    added some inspiration for 404 screen

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+// TODO: Inspiration: https://dribbble.com/shots/3537472-404-Page-for-Digital-Creative-Agency - 
 export default class NotFound extends Component {
     render() {
         return(
