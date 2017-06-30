41adbbc61d435e551c8568307ee3707d1c07ae4a
Anders Blom <anders@andersblom.dk>
2017-06-30 13:16:26 +0200

    Some contact component class'ing

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Contact extends Component {
     render() {
         return(
-            <div className="contentArea contact">
+            <div className="contact">
                 <h1>Can't wait to hear from you!</h1>
                 <div className="contact-email">
                     <i className="fa fa-envelope-o" aria-hidden="true"></i>
