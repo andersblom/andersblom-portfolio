90d18e15083af87b7c805122651eb2b6c3f8b249
Anders Blom <anders@andersblom.dk>
2017-07-11 09:57:09 +0200

    animations for Welcome

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
 import Sidebar from '../Sidebar/Sidebar';
 
 import './Welcome.css';
@@ -17,19 +19,23 @@ export default class Welcome extends Component {
             <div className="container">
                 <Sidebar match={this.props.match} />
                 <div className="contentArea welcome">
-                    <div className="welcome-img"><img className="profilePic" src={profilePic} alt="Anders" /></div>
-                    <h1>Hi there! I'm Anders. 👋</h1>
+                    <ReactCSSTransitionGroup component="div" className="welcome-img" transitionName="fadeIn-delayed" transitionAppearTimeout={0} transitionAppear={true} transitionEnter={false} transitionLeave={false}><img className="profilePic" src={profilePic} alt="Anders" /></ReactCSSTransitionGroup>
+                        <ReactCSSTransitionGroup transitionName="slideIn" transitionAppearTimeout={0} transitionAppear={true} transitionEnter={false} transitionLeave={false}><h1>Hi there! I'm Anders. 👋</h1></ReactCSSTransitionGroup>
+                    
+                    <ReactCSSTransitionGroup component="div" className="welcome-content-wrapper" transitionName="fadeIn-delayed" transitionAppearTimeout={0} transitionAppear={true} transitionEnter={false} transitionLeave={false}>
                     <span className="about">
-                        Web/UI Designer and Front-end Developer from Denmark. I've been at it since back when "optimized for IE in 800x600" was acceptable.
-                        Today, whether I'm pixel-pushing UI, neck-deep in JavaScript MVC, or brainstorming ideas with a team, I still love what I do.
+                        Designer who codes. From Denmark. I've been at it since back when "optimized for IE in 800x600" was okay.
+                        Today, whether I'm pixel-pushing UI, neck-deep in JavaScript-hacking, or brainstorming ideas with a team, I still love what I do.
                     </span>
+                    
                     <h2>What would you like to see?</h2>
                     <span className="no-worries">Don't worry. You can change your mind at any time.<br />
                     Just click these icons in the nav-bar on the left!</span>
                     <div className="welcome-icon-wrapper">
-                        <Link className="designIcon" to="/design"><i className="fa fa-diamond" aria-hidden="true"></i><div>Design</div></Link>
-                        <Link className="codeIcon" to="/code"><i className="fa fa-code" aria-hidden="true"></i><div>Code</div></Link>
+                        <Link className="designIcon" to="/design"><i className="fa fa-diamond" aria-hidden="true"></i><div className="iconExplainer">Design</div></Link>
+                        <Link className="codeIcon" to="/code"><i className="fa fa-code" aria-hidden="true"></i><div className="iconExplainer">Code</div></Link>
                     </div>
+                    </ReactCSSTransitionGroup>
                 </div>
             </div>
         );
