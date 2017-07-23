29a3855279a6794366f95be993163daad02defff
Anders Blom <anders@andersblom.dk>
2017-07-23 18:26:42 +0200

    welcome horizontal centering

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -16,23 +16,25 @@ export default class Welcome extends Component {
                 <Sidebar match={this.props.match} />
                 <div className="contentArea">
                     <div className="welcome">
-                        <ReactCSSTransitionGroup component="div" className="welcome-img" transitionName="fadeIn-delayed" transitionAppearTimeout={0} transitionAppear={true} transitionEnter={false} transitionLeave={false}><img className="profilePic" src={profilePic} alt="Anders" /></ReactCSSTransitionGroup>
-                        <ReactCSSTransitionGroup transitionName="slideIn" transitionAppearTimeout={0} transitionAppear={true} transitionEnter={false} transitionLeave={false}><h1>Hi there! I'm Anders. <span role="img" aria-label="Emoji">👋</span></h1></ReactCSSTransitionGroup>
-                    
-                        <ReactCSSTransitionGroup component="div" className="welcome-content-wrapper" transitionName="fadeIn-delayed" transitionAppearTimeout={0} transitionAppear={true} transitionEnter={false} transitionLeave={false}>
-                        <span className="about">
-                            Designer who codes. From Denmark. I've been at it since back when "optimized for IE in 800x600" was not an internet-crime.
-                            Today, whether I'm pixel-pushing UI, neck-deep in JavaScript-hacking, or brainstorming ideas with a team, I still love what I do.
-                        </span>
+                        <div className="welcomeContent">
+                            <ReactCSSTransitionGroup component="div" className="welcome-img" transitionName="fadeIn-delayed" transitionAppearTimeout={0} transitionAppear={true} transitionEnter={false} transitionLeave={false}><img className="profilePic" src={profilePic} alt="Anders" /></ReactCSSTransitionGroup>
+                            <ReactCSSTransitionGroup transitionName="slideIn" transitionAppearTimeout={0} transitionAppear={true} transitionEnter={false} transitionLeave={false}><h1>Hi there! I'm Anders. <span role="img" aria-label="Emoji">👋</span></h1></ReactCSSTransitionGroup>
                         
-                        <h2>What would you like to see?</h2>
-                        <span className="no-worries">Don't worry. You can change your mind at any time.<br />
-                        Just click these icons in the nav-bar on the left!</span>
-                        <div className="welcome-icon-wrapper">
-                            <Link className="designIcon" to="/design"><i className="fa fa-diamond" aria-hidden="true"></i><div className="iconExplainer">Design</div></Link>
-                            <Link className="codeIcon" to="/code"><i className="fa fa-code" aria-hidden="true"></i><div className="iconExplainer">Code</div></Link>
+                            <ReactCSSTransitionGroup component="div" className="welcome-content-wrapper" transitionName="fadeIn-delayed" transitionAppearTimeout={0} transitionAppear={true} transitionEnter={false} transitionLeave={false}>
+                            <span className="about">
+                                Designer who codes. From Denmark. I've been at it since back when "optimized for IE in 800x600" was not an internet-crime.
+                                Today, whether I'm pixel-pushing UI, neck-deep in JavaScript-hacking, or brainstorming ideas with a team, I still love what I do.
+                            </span>
+                            
+                            <h2>What would you like to see?</h2>
+                            <span className="no-worries">Don't worry. You can change your mind at any time.<br />
+                            Just click these icons in the nav-bar on the left!</span>
+                            <div className="welcome-icon-wrapper">
+                                <Link className="designIcon" to="/design"><i className="fa fa-diamond" aria-hidden="true"></i><div className="iconExplainer">Design</div></Link>
+                                <Link className="codeIcon" to="/code"><i className="fa fa-code" aria-hidden="true"></i><div className="iconExplainer">Code</div></Link>
+                            </div>
+                            </ReactCSSTransitionGroup>
                         </div>
-                        </ReactCSSTransitionGroup>
                     </div>
                 </div>
             </div>
