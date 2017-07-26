d45bce914e1b137d563cd9a17b9764f6b5a94279
Anders Blom <anders@andersblom.dk>
2017-07-26 11:21:57 +0200

    added contact illustration

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 
 import './Contact.css';
 
+import contactIllustration from './contact-illu.png';
+
 export default class Contact extends Component {
     render() {
         return(
             <div className="contact">
                 <div className="contactContent">
+                    <img className="contactIllustration" src={contactIllustration} alt="Contact me" /> 
                     <h1 className="contactHeader">Get in touch</h1>
                     <p>No matter if you want to talk business opportunities, 
                         talk design / code, or just really want to say hello, 
