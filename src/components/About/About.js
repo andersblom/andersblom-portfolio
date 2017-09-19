607827b972bb0936e672425a3f080ebe2e9f6938
Anders Blom <anders@andersblom.dk>
2017-09-19 09:33:37 -0400

    little copy change after big events

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -17,7 +17,7 @@ export default class About extends Component {
         <ReactCSSTransitionGroup transitionName="fadeInUp" transitionAppearTimeout={700} transitionAppear={true} transitionEnter={true} transitionEnterTimeout={500} transitionLeave={false}>
         <div className="aboutSectionContent">
           <h1 className="aboutTitle">Hi there!</h1>
-          <div className="aboutParagraph">My name is Anders Blom. I'm 26 years old, and I was born and raised in Kolding, Denmark. Today, I live in the US (currently Buffalo, New York) with my fiancée and our cat, Monkey.</div>
+          <div className="aboutParagraph">My name is Anders Blom. I'm 26 years old, and I was born and raised in Kolding, Denmark. Today, I live in the US (currently Buffalo, New York) with my wife and our cat, Monkey.</div>
           <div className="aboutParagraph">I &lt;3 digital things. No matter if it's a website, a mobile app or something that appears on a watch, I can't help but explore and look at the nuts and bolts of the technology behind to see how it works. I'm that guy who inspects a website in Chrome Developer Tools before actually reading what's on there.</div>
           <div className="aboutParagraph"><span className="highlight">Im currently on the lookout for new full-time adventures!</span></div>
 
