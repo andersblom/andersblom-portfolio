7e824e31b0492d4851ddb10fd541527dea212a61
Anders Blom <anders@andersblom.dk>
2017-11-02 21:59:38 -0400

    Welcome redone 🎉

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -17,14 +17,13 @@ export default class Welcome extends Component {
                 <div className="contentArea">
                     <div className="welcome">
                         <div className="welcomeContent">
-                            {/* <ReactCSSTransitionGroup component="div" className="welcome-img" transitionName="fadeIn-delayed" transitionAppearTimeout={0} transitionAppear={true} transitionEnter={false} transitionLeave={false}><img className="profilePic" src={profilePic} alt="Anders" /></ReactCSSTransitionGroup> */}
                             <ReactCSSTransitionGroup transitionName="slideIn" transitionAppearTimeout={0} transitionAppear={true} transitionEnter={false} transitionLeave={false}>
                                 <h1 className="welcometitle">Hi there, new friend! I'm Anders. <span role="img" aria-label="Waving emoji">👋</span></h1>
                             </ReactCSSTransitionGroup>
                         
                             <ReactCSSTransitionGroup component="div" className="welcome-content-wrapper" transitionName="fadeIn-delayed" transitionAppearTimeout={0} transitionAppear={true} transitionEnter={false} transitionLeave={false}>
                                 <h2 className="subHeader">
-                                    I'm a designer who codes. And a developer who designs. <br />Born and raised in Kolding, Denmark. Now living in the United States.
+                                Developer/Designer-hybrid currently based in Buffalo, NY
                                 </h2>
                                 <span className="about">
                                     I've been at it since back when putting "optimized for IE in 800x600" in your footer was not considered an internet crime.
@@ -32,12 +31,10 @@ export default class Welcome extends Component {
                                 </span>
                                 
                                 <h2 className="catPicker">What work would you like to see?</h2>
-                                <div className="no-worries">(You can click these icons in the <span className="sidebar-text">sidebar</span><span className="header-text">header</span> too!)</div>
                                 <div className="welcome-icon-wrapper">
-                                    <Link className="designIcon" to="/design"><i className="fa fa-diamond" aria-hidden="true"></i><div className="iconExplainer">Design</div></Link>
-                                    <Link className="codeIcon" to="/code"><i className="fa fa-code" aria-hidden="true"></i><div className="iconExplainer">Code</div></Link>
+                                    <Link className="designIcon" to="/design"><i className="fa fa-diamond" aria-hidden="true"></i><div className="iconExplainer">Design</div><div className="iconExplainer--hover">Design</div></Link>
+                                    <Link className="codeIcon" to="/code"><i className="fa fa-code" aria-hidden="true"></i><div className="iconExplainer">Code</div><div className="iconExplainer--hover">Code</div></Link>
                                 </div>
-                                <div className="extraProjectsTeaser">If you want to see more things from me, I'm throwing a few shots over at <a target="_blank" rel="noopener noreferrer" href="http://andersblom.dribbble.com/">Dribbble</a> or committing to projects at <a target="_blank" rel="noopener noreferrer" href="https://github.com/andersblom">Github</a>.</div>
                             </ReactCSSTransitionGroup>
                         </div>
                     </div>
